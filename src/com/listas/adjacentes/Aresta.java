89be8963c56bf64db3a9cfb9d28a1036bb934b90
Leonardo Menezes <leonhard.menezes@gmail.com>
2020-11-26 19:56:42 -0300

    Removendo comentários desnecessários

--- src/com/listas/adjacentes/Aresta.java
+++ src/com/listas/adjacentes/Aresta.java
@@ -1,11 +1,9 @@
 package com.listas.adjacentes;
 
 public class Aresta {
-	// public Vertice origem;
 	public Vertice destino;
 
     Aresta(Vertice destino) {
-    	// this.origem = origem;
         this.destino = destino;
     }
 }
