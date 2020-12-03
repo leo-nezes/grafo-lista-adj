b96669f3206734a62eda0d93ad28f8df6a2c2dbd
Leonardo Menezes <leonhard.menezes@gmail.com>
2020-12-02 21:02:24 -0300

    Ajustes BFS

--- src/com/buscas/largura/BFS.java
+++ src/com/buscas/largura/BFS.java
@@ -8,44 +8,51 @@ import com.listas.adjacentes.*;
 public class BFS {
 	private List<Vertice> fila;
 	private String resultado;
+	private String auxCor;
 
 	public BFS() {
 		this.fila = new ArrayList<Vertice>();
 		this.resultado = "";
+		this.auxCor = "b";
 	}
 	
 	public void iniciarBusca(Vertice vertice, boolean verticeOrigem) {
-		resultado += "---------------------------------\n";
-		resultado += "BFS(" + vertice.getNome() + ")\n";
-		
 		if(verticeOrigem) fila.add(vertice);
 		
 		Vertice newVertice = fila.get(0);
 		
 		newVertice.setCor("c");
 		
-		resultado += (newVertice.getPi() == null) ? "Antecessor: \n" : "Antecessor: " + newVertice.getPi() + "\n"; 
-				
-		resultado += "Cor: " + newVertice.getCor() + "\n";
-		
-		if (verticeOrigem) newVertice.settInicial(0);
-		
 		newVertice.settInicial((verticeOrigem) ? 0 : newVertice.getPi().gettInicial() + 1);
-
-		resultado += "Tempo Inicial: " + newVertice.gettInicial() + "\n";
+		
+		setResultado(newVertice);
 		
 		newVertice.getAdjacencias().forEach(adjacencia -> {
-			adjacencia.getDestino().setPi(newVertice);
-			
-			fila.add(adjacencia.getDestino());
+			if (adjacencia.getDestino().getCor() == "b") {
+				adjacencia.getDestino().setPi(newVertice);
+				
+				fila.add(adjacencia.getDestino());
+			}
 		});
 		
 		newVertice.setCor("p");
 		
+		setResultado(newVertice);
+		
 		fila.remove(0);
 		
-		iniciarBusca(fila.get(0), false);
-		
+		if (!fila.isEmpty()) {
+			
+			iniciarBusca(fila.get(0), false);
+		}
+	}
+	
+	private void setResultado(Vertice vertice) {
+		this.resultado += "---------------------------------\n";
+		this.resultado += "BFS(" + vertice.getNome() + ")\n";
+		this.resultado += "Cor: " + vertice.getCor() + "\n";
+		this.resultado += (vertice.getPi() == null) ? "Antecessor: \n" : "Antecessor: " + vertice.getPi().getNome() + "\n";
+		this.resultado += "Tempo Inicial: " + vertice.gettInicial() + "\n";
 	}
 	
 	public String toString() {
--- src/com/buscas/largura/Main.java
+++ src/com/buscas/largura/Main.java
@@ -8,15 +8,27 @@ public class Main {
 		Grafo grafo = new Grafo();
 		BFS dfs = new BFS();
 		
-		Vertice a = grafo.addVertice("A");
-	    Vertice b = grafo.addVertice("B");
-	    Vertice c = grafo.addVertice("C");
+		Vertice v = grafo.addVertice("V");
+		Vertice r = grafo.addVertice("R");
+	    Vertice s = grafo.addVertice("S");
+	    Vertice w = grafo.addVertice("W");
+	    Vertice t = grafo.addVertice("T");
+	    Vertice x = grafo.addVertice("X");
+	    Vertice u = grafo.addVertice("U");
+	    Vertice y = grafo.addVertice("Y");
 	    
-	    Aresta ab = grafo.addAresta(a, b);
-	    Aresta ac = grafo.addAresta(a, c);
+	    Aresta sr = grafo.addAresta(s, r);
+	    Aresta sw = grafo.addAresta(s, w);
+	    Aresta rv = grafo.addAresta(r, v);
+	    Aresta wt = grafo.addAresta(w, t);
+	    Aresta wx = grafo.addAresta(w, x);
+	    Aresta tu = grafo.addAresta(t, u);
+	    Aresta xy = grafo.addAresta(x, y);
 	    
-	    dfs.iniciarBusca(grafo.getVertices().get(0), true);
 	    
+	    dfs.iniciarBusca(s, true);
+	    
+	    // System.out.println(grafo);
 	    System.out.println(dfs);
 
 	}
